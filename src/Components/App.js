ef0ab71cdf964e9ca8436042e3bb056b9922b288
normanlikescats <normankwok97@gmail.com>
2023-03-09 14:50:34 +0800

    set up router and basic components to build out

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,36 @@
 import React from "react";
-import logo from "./logo.png";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import LandingPage from "./LandingPage"
+import Login from "./Login"
+import Register from "./Register"
+import Feed from "./Feed"
+import Error from "./Error"
+import Movie from "./Movie"
+import Profile from "./Profile"
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element= {<LandingPage />}/>
+              <Route path="/login" element= {<Login />}/>
+              <Route path="/register" element= {<Register/>}/>
+              <Route path="/feed" element= {<Feed />}/>
+              <Route path="*" element= {<Error/>}/>
+              <Route path="/:profileId/:profilename" element= {<Profile/>}/>
+              <Route path="/:movieId/:movietitle" element= {<Movie/>}/>
+            </Routes>            
+          </BrowserRouter>
+      </header>
+    </div>
+  );
 }
 
 export default App;
