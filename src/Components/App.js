483573a3e87966d1dd83f36c2fefa2ee740752eb
Sandiprumba <sandiprumba.lama@gmail.com>
2023-03-21 17:38:20 +0800

    Add-sidebar-data

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,16 @@
 import React from "react";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
-import LandingPage from "./LandingPage"
-import Login from "./Login"
-import Feed from "./Feed"
-import Error from "./Error"
-import Movie from "./Movie"
-import CreateProfile from "./CreateProfile"
-import ReviewCreator from "./ReviewCreator"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import Login from "./Login";
+import Feed from "./Feed";
+import Error from "./Error";
+import Movie from "./Movie";
+import CreateProfile from "./CreateProfile";
+import ReviewCreator from "./ReviewCreator";
 import Profile from "./Profile";
 import "./App.css";
 import { AuthContextProvider } from "../Context/AuthContext";
-
+import Navbar from "./NavBar";
 
 function App() {
   return (
@@ -22,18 +18,20 @@ function App() {
       <header className="App-header">
         <AuthContextProvider>
           <BrowserRouter>
+            <Navbar />
             <Routes>
-              <Route exact path="/" element= {<LandingPage />}/>
-              <Route path="/login" element= {<Login />}/>
-              <Route path="/feed" element= {<Feed />}/>
-              <Route path="*" element= {<Error/>}/>
-              <Route path="/create-profile" element= {<CreateProfile/>}/>
-              <Route path="/profile/:userId" element= {<Profile/>}/>
-              <Route path="/movie/:movieId/:movietitle" element= {<Movie/>}/>
-              <Route path="/create-review" element = {<ReviewCreator/>}/>
+              <Route exact path="/" element={<LandingPage />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/feed" element={<Feed />} />
+              <Route path="*" element={<Error />} />
+              <Route path="/create-profile" element={<CreateProfile />} />
+              <Route path="/profile/:userId" element={<Profile />} />
+              <Route path="/movie/:movieId/:movietitle" element={<Movie />} />
+              <Route path="/create-review" element={<ReviewCreator />} />
+              <Route path="/" />
             </Routes>
           </BrowserRouter>
-        </AuthContextProvider>            
+        </AuthContextProvider>
       </header>
     </div>
   );
