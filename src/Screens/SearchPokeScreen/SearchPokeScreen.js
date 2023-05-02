ba8843994d06a936291d759fdeffcdaf78fa1c7c
markcwy-ra <rocket@markcwy.com>
2023-05-02 16:28:00 +0800

    Fixed object writing issue

--- src/Screens/SearchPokeScreen/SearchPokeScreen.js
+++ src/Screens/SearchPokeScreen/SearchPokeScreen.js
@@ -5,82 +5,74 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 
 import {
-  faPlus,
-  faStar,
   faMagnifyingGlass,
   faChevronLeft,
 } from "@fortawesome/free-solid-svg-icons";
 import { get, ref, set } from "firebase/database";
 import { database } from "../../firebase";
-import { UserContext } from "../../App";
-
-const pokeObj = {
-  type: null,
-  imgURL: null
-}
+import { NavContext, UserContext } from "../../App";
 
 //comment
 const SearchPokeScreen = ({ DB_USERS_KEY }) => {
   const [input, setInput] = useState("");
-  const [pokeData, setPokeData] = useState(pokeObj);
+  const [pokeData, setPokeData] = useState(null);
   const [pokeName, setPokeName] = useState("");
   const { user } = useContext(UserContext);
+  const { navigate } = useContext(NavContext);
 
   const handleChange = (e) => {
     setInput(e.target.value);
-  }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${input}`)
-      .then((response) => {
-        setPokeData()
-        const retrievedData = {
-          type: response.data.types[0].type.name,
-          imgURL: response.data.sprites.front_default
-        }
-        console.log(retrievedData)
-        setPokeName(response.data.name) //storing the response data as a state
-        setPokeData(retrievedData); // --ditto--
-      });
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`).then((response) => {
+      // setPokeData()
+      const retrievedData = {
+        type: response.data.types[0].type.name,
+        imgURL: response.data.sprites.front_default,
+      };
+      setPokeName(response.data.name); //storing the response data as a state
+      setPokeData(retrievedData); // --ditto--
+    });
   };
 
-
   const handleClick = (e) => {
-    const pokeRef = ref(database, DB_USERS_KEY + "/" + user.uid + "/" + e.target.id + "/" + pokeName)
-    const listOrderRef = ref(database, DB_USERS_KEY + "/" + user.uid + "/" + e.target.id + "order")
-    if (e.target.id == "topten") {
-      get(pokeRef).then((data) => {
-        if (data.exists()) {
-          alert('pokemon in list')
-        } else {
-          set(pokeRef, pokeData)  //setPokeData(retrievedData);
-            .then(() => {
-              get(listOrderRef).then((data) => {
-                if (data.exists()) {
-                  const newList = [...data.val(), pokeName]
-                  set(listOrderRef, newList)
-                } else {
-                  set(listOrderRef, [pokeName])
-                }
-
-              })
-            })
-        }
-
-
-      })
-
-
-
-    } else if (e.target.id == "wishlist") {
-
-    }
+    console.log(e.target);
+    const pokeRef = ref(
+      database,
+      DB_USERS_KEY + "/" + user.uid + "/" + e.target.id + "/" + pokeName
+    );
+    const listOrderRef = ref(
+      database,
+      DB_USERS_KEY + "/" + user.uid + "/" + e.target.id + "order"
+    );
+    get(pokeRef).then((data) => {
+      if (data.exists()) {
+        alert("pokemon in list");
+      } else {
+        set(pokeRef, pokeData) //setPokeData(retrievedData);
+          .then(() => {
+            get(listOrderRef).then((data) => {
+              if (data.exists()) {
+                const newList = [...data.val(), pokeName];
+                set(listOrderRef, newList);
+              } else {
+                set(listOrderRef, [pokeName]);
+              }
+            });
+          });
+      }
+    });
   };
   return (
     <div className="pokeSearch">
-      <button className="back">
+      <button
+        className="back"
+        onClick={() => {
+          navigate("/profile");
+        }}
+      >
         <FontAwesomeIcon icon={faChevronLeft} size="2xl" />
       </button>
 
@@ -103,24 +95,23 @@ const SearchPokeScreen = ({ DB_USERS_KEY }) => {
         </div>
 
         <br />
-        {/* <h1>{pokeData}</h1> */}
 
         <div>
           <div className="flex-container">
-            <div className="flex-item results">{pokeData}</div>
+            <div className="flex-item results">{pokeName}</div>
             <button
               className="flex-item plus"
               onClick={handleClick}
               id="topten"
             >
-              <FontAwesomeIcon icon={faPlus} />
+              +
             </button>
             <button
               className="flex-item star"
               onClick={handleClick}
               id="wishlist"
             >
-              <FontAwesomeIcon icon={faStar} />
+              ★
             </button>
           </div>
         </div>
