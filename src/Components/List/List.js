0e6c7863b636bd22fb1ec6fec0a044827c0efb13
markcwy-ra <rocket@markcwy.com>
2023-05-04 09:47:30 +0800

    Tried something that didn't work in List.js

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -4,23 +4,20 @@ import { NavContext } from "../../App";
 
 const List = ({ list, listOrder, id }) => {
   const { navigate } = useContext(NavContext);
-  let displayList = listOrder.map((pokemon, index) => {
-    return (
-      <button
-        className="list-item"
-        key={pokemon}
-        onClick={() => {
-          navigate("profile/" + id + "-" + pokemon);
-        }}
-      >
-        <h2>{index + 1}</h2>
+  let displayList = listOrder.map((pokemon, index) => (
+    <button
+      className="list-item"
+      key={pokemon}
+      onClick={() => {
+        navigate("profile/" + id + "-" + pokemon);
+      }}
+    >
+      <h2>{index + 1}</h2>
 
-        <h1>{pokemon}</h1>
-        <img src={list[pokemon].imgURL} alt={pokemon} />
-      </button>
-    );
-  });
-
+      <h1>{pokemon}</h1>
+      <img src={list[pokemon].imgURL} alt={pokemon} />
+    </button>
+  ));
   return <div id="list">{displayList}</div>;
 };
 
