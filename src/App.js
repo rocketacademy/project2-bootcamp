fb5895b73323cfcd9eb8ee383dd26480bbd40c2c
calebcianc <calebcc.castro@gmail.com>
2023-06-10 12:38:09 +0800

    Changed favicon files and title

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+        Welcome to Dollar Direction! <br />
+        Now you will always know 'where'd your money go'!
       </div>
     );
   }
