7ee9ab838fabe0ebf6028ab72fb83a723f668a8c
khloeli <chloelihuimin@gmail.com>
2023-06-17 07:27:36 +0800

    edit welcome page to show mapexpense if loggedin

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,21 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "../App.css";
 
-export default function Welcome() {
+export default function Welcome({ isLoggedIn }) {
+  const navigate = useNavigate();
   return (
     <div>
-      <div className="temporary-box">
-        <div>
-          Welcome to DollarDirection - the app that keeps track of where you've
-          been and how much you've spent so you will never have to ask{" "}
-          <b>
-            <em>"Where'd my money go?!"</em>
-          </b>{" "}
-          <br />
-          <br />
-          <Link to="/authform">Sign Up / Log In here</Link>
+      {isLoggedIn ? (
+        navigate("/mapexpenses")
+      ) : (
+        <div className="temporary-box">
+          <div>
+            Welcome to DollarDirection - the app that keeps track of where
+            you've been and how much you've spent so you will never have to ask{" "}
+            <b>
+              <em>"Where'd my money go?!"</em>
+            </b>{" "}
+            <br />
+            <br />
+            <Link to="/authform">Sign Up / Log In here</Link>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
