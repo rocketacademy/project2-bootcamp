c6187eed347d534297ba75d7ce636c7647369d49
alaynelxr <alaynelxr@gmail.com>
2023-06-23 09:11:27 +0800

    added search bar and page results. used spoonacular api

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+function Search() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate("/search/" + input);
+  };
+
+  return (
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <FaSearch></FaSearch>
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Enter a search"
+          value={input}
+        />
+        <p>You're searching for "{input}"...</p>
+      </div>
+    </FormStyle>
+  );
+}
+
+const FormStyle = styled.form`
+  margin: 0rem 20rem;
+  div {
+    position: relative;
+    width: 100%;
+  }
+  input {
+    border: none;
+    background: linear-gradient(35 deg, #494949, #313131);
+    font-size: 1.5rem;
+    color: black;
+    border-radius: 1 rem;
+    outline: none;
+    width: 100%;
+  }
+  svg {
+    position: absolute;
+    top: 50% left 0%;
+    transform: translate(100%, -50%);
+    color: white;
+  }
+`;
+
+export default Search;
