8aa45ef84c859f774780d8fd0b1d58cb6b2f2347
JerryTohHS <jerrytoh93@gmail.com>
2023-06-24 10:05:05 +0800

    jerry-add trending carousel and resultrecipe page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import logo from "./assets/logo.png";
 import "./App.css";
 import defineRoutesHere from "./routes/routes";
 import ResponsiveAppBar from "./components/Navbar";
+import Trending from "./components/Trending";
+import ResultRecipe from "./pages/ResultRecipe";
 
 const App = () => {
   return (
     <div className="App">
-      <ResponsiveAppBar/>
+      <ResponsiveAppBar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <br/>
+        <Trending />
+        <ResultRecipe />
+        <br />
         {defineRoutesHere()}
       </header>
     </div>
