ff8d06eff3642a71900ad60786654e52a58dd26e
JerryTohHS <jerrytoh93@gmail.com>
2023-07-01 09:50:18 +0800

    styling

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import logo from "../assets/banner.png";
 
 function Trending() {
   const [recipes, setRecipes] = useState([]);
+  const splideRef = useRef(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY2}&number=1`
+          `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=3`
         );
         console.log(response.data);
-        
+
         setRecipes(response.data.recipes);
       } catch (error) {
         console.error("Error fetching recipes:", error);
@@ -26,36 +29,57 @@ function Trending() {
   }, []);
 
   const Wrapper = styled.div`
-    margin: 1rem 0 rem;
+    margin: 0;
+    padding: 0;
+  `;
+
+  const LogoWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    margin-top: -4rem;
+    padding: 0;
+  `;
+
+  const Logo = styled.img`
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+    margin-bottom: 0rem;
+    padding: 0;
+    border-radius: 10rem;
   `;
 
   const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
-
-  img {
+    min-height: 25rem;
     border-radius: 2rem;
-  }
+    overflow: hidden;
+    position: relative;
 
-  p {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 20%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 10%
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
+    img {
+      border-radius: 2rem;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    p {
+      position: absolute;
+      z-index: 10;
+      left: 50%;
+      bottom: 0%;
+      transform: translate(-50%, 0%);
+      color: white;
+      width: 100%;
+      text-align: center;
+      font-weight: 300;
+      font-size: 1.25rem;
+      font-family: "Gill Sans", sans-serif;
+      height: 10%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  `;
 
   const Gradient = styled.div`
     z-index: 3;
@@ -65,17 +89,73 @@ function Trending() {
     background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
   `;
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        splideRef.current.options = {
+          perPage: 1,
+          pagination: false,
+          drag: true,
+          gap: "1rem",
+        };
+      } else {
+        splideRef.current.options = {
+          perPage: 3,
+          pagination: false,
+          drag: "free",
+          gap: "1rem",
+        };
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const HeadingContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 1rem;
+  `;
+
+  const HeadingText = styled.h2`
+    margin-top: -7rem;
+  `;
+
+  const SubHeadingText = styled.h1`
+    margin-left: 2rem;
+    margin-bottom: 1rem;
+    text-align: left;
+  `;
+
   return (
-    <div>
-      <h4>Trending</h4>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <LogoWrapper>
+        <Logo src={logo} alt="Logo" />
+      </LogoWrapper>
+      <HeadingContainer>
+        <HeadingText></HeadingText>
+      </HeadingContainer>
+      <SubHeadingText>Popular Picks Today!</SubHeadingText>
       <Wrapper>
         <Splide
           options={{
-            perPage: 2,
+            perPage: 3,
             pagination: false,
             drag: "free",
             gap: "1rem",
           }}
+          ref={splideRef}
         >
           {recipes.map((recipe) => (
             <SplideSlide key={recipe.id}>
@@ -90,7 +170,7 @@ function Trending() {
           ))}
         </Splide>
       </Wrapper>
-    </div>
+    </motion.div>
   );
 }
 
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Search from "../components/SearchBar";
+import { motion } from "framer-motion";
 
 // mui styling
 import Card from "@mui/material/Card";
@@ -11,15 +12,16 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
+import Footer from "../components/Footer";
 
-function SearchPage() {
+function SearchPage({ isHomePage }) {
   const [searchedRecipes, setSearchedRecipes] = useState([]);
 
   let params = useParams();
 
   const getSearchResults = async (name) => {
     const data = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY2}&query=${name}&number=4`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query=${name}&number=4`
     );
     const recipes = await data.json();
     setSearchedRecipes(recipes.results);
@@ -32,7 +34,7 @@ function SearchPage() {
   return (
     <div>
       <Search></Search>
-      <Grid container spacing={2}>
+      <Grid container spacing={1}>
         {searchedRecipes.map((item) => {
           return (
             <Grid
@@ -42,46 +44,52 @@ function SearchPage() {
               justifyContent="center"
               alignItems="center"
             >
-              <Card sx={{ maxWidth: 345 }} key={item.id}>
-                <CardActionArea
-                  component={Link}
-                  to={`/recipe/${item.id}`}
+              <motion.div
+                animate={{ opacity: 1 }}
+                initial={{ opacity: 0 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+              >
+                <Card
+                  sx={{
+                    maxWidth: 345,
+                    backgroundColor: "#386150",
+                    borderRadius: "0.5rem",
+                    border: "1px solid rgba(0, 0, 0, 0.2)",
+                    boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)",
+                  }}
                   key={item.id}
                 >
-                  <CardMedia component="img" src={item.image} alt="" />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {item.title}
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
+                  <CardActionArea
+                    component={Link}
+                    to={`/recipe/${item.id}`}
+                    key={item.id}
+                  >
+                    <CardMedia component="img" src={item.image} alt="" />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="h5"
+                        component="div"
+                        sx={{
+                          fontFamily: "gill sans, sans-serif",
+                          fontSize: "1.5rem",
+                          color: "white",
+                        }}
+                      >
+                        {item.title}
+                      </Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </Card>
+              </motion.div>
             </Grid>
           );
         })}
       </Grid>
+      {!isHomePage && <Footer />}
     </div>
   );
 }
-
-// const Grid = styled.div`
-//   display: grid;
-//   grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
-//   grip-gap: 3rem;
-// `;
-
-// const Card = styled.div`
-// img
-//   {
-//   width:100%
-//   border-radius: 2rem;
-// }
-// a
-// {
-//   text-decoration:none;}
-// h4
-// {text-align:center;
-// padding: 1 rem;
-// }`;
 
 export default SearchPage;
