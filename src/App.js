6e838e1a1e69ed5598d185b033bf218db699d9a3
dexterch91 <dexterchewxh@hotmail.sg>
2023-07-18 02:36:04 +0800

    Added ADMIN USER email for upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 //routes
 import AdminUpload from "./components/AdminUpload";
@@ -9,17 +9,34 @@ import Home from "./components/Home";
 import SignIn from "./components/LoginDefault";
 import SignUp from "./components/SignUpAccount";
 import Auth from "./components/Auth";
+import ErrorPage from "./components/ErrorPage";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+const PrivateAdminRoute = (user) => {
+  const ADMIN_USER = "speed6200@gmail.com";
+  try {
+    // Code that may throw an error or exception
+    if (user.email === ADMIN_USER) {
+      return <AdminUpload />;
+    }
+  } catch (error) {
+    // Code to handle the error
+    console.error("An error occurred:", error);
+    return <ErrorPage />;
+  }
+  return <ErrorPage />
+};
 
 const App = () => {
   const auth = getAuth();
+  
   const [loggedInUser, setLoggedInUser] = React.useState(null);
 
   React.useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       console.log("Auth State Triggered");
       if (user) {
-        console.log(`User Registered: ${user}`);
+        console.log(`User Registered: ${user.email}`);
         setLoggedInUser(user);
       } else {
         setLoggedInUser(null);
@@ -29,7 +46,7 @@ const App = () => {
     return () => {
       unsubscribe();
     };
-  }, []);
+  });
 
   return (
     <BrowserRouter>
@@ -42,7 +59,7 @@ const App = () => {
                 path="/"
                 element={auth.currentUser ? <Home /> : <SignIn />}
               />
-              <Route exact path="/admin" element={<AdminUpload />} />
+              <Route exact path="/admin" element={PrivateAdminRoute(loggedInUser)}/>;
               <Route exact path="/login" element={<SignIn />} />
               <Route exact path="/signup" element={<SignUp />} />
             </Routes>
---
+++ src/components/ErrorPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Box, Typography, Button, Icon } from '@mui/material';
+import { ArrowBack } from '@mui/icons-material';
+import {useNavigate} from "react-router-dom";
+import ErrorIcon from '@mui/icons-material/Error';
+
+const ErrorPage = () => {
+    const errorCode = "401";
+    const errorMessage = "Unauthorised User!"
+    const navigate = useNavigate();
+    const onBackButtonClick = () => {
+        // Perform any necessary actions before navigation
+        // ...
+        // Navigate back to the previous page
+        navigate(-1);
+      };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+        <Icon sx={{ fontSize: '88px' }}>
+          <ErrorIcon fontSize="inherit" color="warning" />
+        </Icon>
+
+      <Typography variant="h4" gutterBottom>
+        Error {errorCode}
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        {errorMessage}
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<ArrowBack />}
+        onClick={onBackButtonClick}
+        sx={{ margin: '16px auto' }}
+      >
+        Back
+      </Button>
+    </Box>
+  );
+};
+
+export default ErrorPage;
