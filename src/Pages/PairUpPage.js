b68b8c5aae4245b60deff423c9b87a95d38b0b06
gbrllim <gbrllim@gmail.com>
2023-10-06 16:25:14 +0800

    Finished template for create room , join room and waiting room

--- src/Pages/PairUpPage.js
+++ src/Pages/PairUpPage.js
@@ -1,84 +1,128 @@
 import { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import morty from "../Images/morty.png";
+import heart from "../Images/heart.gif";
 
 export default function PairUp() {
-  const [pairKey, setPairKey] = useState("");
-  const [keyCreated, setKeyCreated] = useState(false); //temp pair key till able to import from database
+  const [pairKey1, setPairKey1] = useState("");
+  const [pairKey2, setPairKey2] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [copied, setCopied] = useState(false);
 
   const navigate = useNavigate();
 
-  const deletePairKey = () => {
-    console.log("Account deleted");
-    //delete user files
-    //sign out user
-    //set global state back to isSignedIn (false) + isPaired (false)
-    navigate("/onboarding");
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText("rick&morty");
+    setCopied(true);
   };
 
   return (
     <div className=" flex h-screen flex-col items-center justify-center">
       <header className="fixed top-0 flex w-screen flex-row items-center justify-between p-4">
-        <NavLink to="/sign-up" className="text-[2em]">
+        <NavLink to="/sign-in" className="text-[2em]">
           ←
         </NavLink>
-        <p className="text-[2em]"></p>
+        <p className="text-[2em]">Pair Up</p>
         <p className="text-transparent">blank</p>
       </header>
-      {keyCreated ? (
-        <main className="flex w-screen flex-col items-center">
-          <h1 className="text-xl font-bold">Waiting room</h1>
-          <p className="mx-5">
-            Once your partner has entered your pair key you'll be able to launch
-            the app!
-          </p>
-          <NavLink to="/sign-in" className="btn m-2">
-            Launch
-          </NavLink>
-        </main>
-      ) : (
-        <main className="flex flex-col items-center">
-          <div className="flex flex-row">
-            <img
-              src={morty}
-              alt="Profile"
-              className="m-1 h-[8em] rounded-full border-2 border-black p-1"
-            />
-            <img
-              src={morty}
-              alt="Profile"
-              className="m-1 h-[8em] rounded-full border-2 border-black p-1"
-            />
+      <div className="flex flex-row">
+        <img
+          src={morty}
+          alt="Profile"
+          className="m-1 h-[8em] rounded-full border-2 border-black p-1"
+        />
+        <img
+          src={morty}
+          alt="Profile"
+          className="m-1 h-[8em] rounded-full border-2 border-black p-1"
+        />
+      </div>
+      {/* Create room */}
+      <main className="m-2 flex w-[30em] flex-col items-center rounded-lg border-[1px] border-slate-800  p-2">
+        <p className="mx-5 my-2 font-bold">
+          Create a unique pair key for you and your partner
+        </p>
+        <form className="flex flex-col items-center">
+          <label>Create Pair Key:</label>
+          <input
+            type="text"
+            className=" mb-2 w-[10em] rounded-md border-[1px] border-black px-2"
+            id="create-pairkey"
+            value={pairKey1}
+            onChange={(e) => {
+              setPairKey1(e.target.value);
+            }}
+            placeholder="rick&morty"
+          />
+          <label>Start of relationship:</label>
+          <input
+            type="date"
+            className="mb-2 w-[10em] rounded-md border-[1px] border-black px-2"
+            id="startDate"
+            value={startDate}
+            onChange={(e) => {
+              setStartDate(e.target.value);
+            }}
+          />
+        </form>
+        {/* Waiting room modal */}
+        <button
+          className="btn w-[10em]"
+          onClick={() => document.getElementById("waiting-room").showModal()}
+        >
+          create room
+        </button>
+        <dialog id="waiting-room" className="modal">
+          <div className="modal-box flex flex-col items-center bg-slate-100">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+                ✕
+              </button>
+            </form>
+            <img src={heart} alt="heart" className="h-[8em]" />
+            {copied ? (
+              <p className="text-green-800">Pair key copied!</p>
+            ) : (
+              <p>Click to copy:</p>
+            )}
+            <button
+              className="rounded-lg bg-slate-200 p-2 text-lg font-bold shadow-md active:translate-y-[3px]"
+              onClick={copyToClipboard}
+            >
+              📑 rick&morty
+            </button>
+            <p className="py-3 text-center">
+              Once your partner has entered your pair key you'll be put together
+              in the room.
+            </p>
+            <NavLink to="/" className="btn m-2">
+              Launch
+            </NavLink>
           </div>
-          <p className="mx-5 my-2 bg-slate-200">
-            Create a unique pair key for you and your partner, once your
-            partnered has entered the pair key - you'll be put together!
-          </p>
-
-          <form className="w-3/4">
-            <label>Create Pair Key:</label>
-            <br />
-            <input
-              type="text"
-              className=" mb-2 w-full rounded-md border-[1px] border-black"
-              id="displayName"
-              placeholder=""
-            />
-
-            <br />
-            <label>Start of relationship:</label>
-            <input
-              type="date"
-              className="mb-2 w-full rounded-md border-[1px] border-black"
-              id="background photo"
-              placeholder="Insert file"
-            />
-          </form>
-          <button className="btn w-1/2" onClick={setKeyCreated}>
-            Create Room
-          </button>
-        </main>
-      )}
+        </dialog>
+      </main>
+      {/* Join room */}
+      <main className="m-2 flex w-[30em] flex-col items-center rounded-lg border-[1px] border-slate-800  p-2">
+        <p className="mx-5 my-2 font-bold">
+          Already have a pair key? Enter your key below
+        </p>
+        <form className="flex flex-col items-center">
+          <label>Enter your Pair Key:</label>
+          <input
+            type="text"
+            className=" mb-2 w-[10em] rounded-md border-[1px] border-black"
+            id="join-pairkey"
+            value={pairKey2}
+            onChange={(e) => {
+              setPairKey2(e.target.value);
+            }}
+          />
+        </form>
+        <NavLink to="/" className="btn w-[10em]">
+          Join Room
+        </NavLink>
+      </main>
     </div>
   );
 }
