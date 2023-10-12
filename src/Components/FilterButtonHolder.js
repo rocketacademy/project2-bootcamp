40369defceecff7c32cab540ad5350574096310c
kenleeyx <kenleeyx@gmail.com>
2023-10-12 09:59:55 +0800

    1210

--- src/Components/FilterButtonHolder.js
+++ src/Components/FilterButtonHolder.js
@@ -1,20 +1,48 @@
-//to add clicked/unclicked status later on
-import {FilterButton} from './FilterButton'
-//<FilterButtonHolder currentFilter={tagFilter} filters={availableFilters} setTagFilter = {setTagFilter}/>
-export function FilterButtonHolder(props) {
-    const filtersArray =  [...props.filters]
-    const FilterButtons = filtersArray.map((filterTag) => (
-        <FilterButton filterTag = {filterTag} setTagFilter = {props.setTagFilter} currentFilters = {props.currentFilters} />
-        //<button onClick={handleFilterButton} className = 'bg-red-300' key = {filterTag} id = {filterTag}>{filterTag}</button>
-    ))
 
-    FilterButtons.push(
-        <FilterButton filterTag = 'Clear' setTagFilter = {props.setTagFilter} currentFilters = {props.currentFilters} />
-    )
+import { useState } from "react"
+import {FilterButton} from './FilterButton'
+
+//<FilterButtonHolder tagFilter={tagFilter} availableFilters={availableFilters} setTagFilter = {setTagFilter}/>
+export function FilterButtonHolder(props) {
+    const [filterText, setFilterText] = useState('')
+    //const filtersArray =  [...props.filters]
+    // const FilterButtons = filtersArray.map((filterTag) => (
+    //     <FilterButton filterTag = {filterTag} setTagFilter = {props.setTagFilter} tagFilter = {props.tagFilter />
+    //     //<button onClick={handleFilterButton} className = 'bg-red-300' key = {filterTag} id = {filterTag}>{filterTag}</button>
+    // ))
+
+    // FilterButtons.push(
+    //     <FilterButton filterTag = 'Clear' setTagFilter = {props.setTagFilter} tagFilter = {props.tagFilter} />
+    // )
+
+    const filterChange = (e) => {
+        setFilterText(e.target.value);
+        props.setCustomFilter(e.target.value)
+      };
 
     return (
     <div className = 'flex justify-center'>
-        {FilterButtons}
+        {/* {FilterButtons} */}
+        <FilterButton filterTag = 'Dates' setTagFilter = {props.setTagFilter} tagFilter = {props.tagFilter} />
+        <FilterButton filterTag = 'Milestones' setTagFilter = {props.setTagFilter} tagFilter = {props.tagFilter} />
+        <input
+          type="text"
+          id="enterFilter"
+          placeholder="enter custom filter:"
+          onChange={(e) => {
+            filterChange(e);
+          }}
+          value={filterText}
+          className="text-black"
+        />
+        <FilterButton 
+        filterTag = 'Clear' 
+        setTagFilter = {props.setTagFilter} 
+        tagFilter = {props.tagFilter} 
+        setFilterText = {setFilterText} 
+        setCustomFilter = {props.setCustomFilter}
+            
+        />
     </div>
     )
 }
