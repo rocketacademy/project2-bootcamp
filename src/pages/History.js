1bc73ae8df71e0bf7d46495f5f20e56439918141
patrickkok <pkok1996@gmail.com>
2023-12-17 19:04:31 +0800

    Create dummy data, create trade history line item, create History.css

--- src/pages/History.js
+++ src/pages/History.js
@@ -1,5 +1,46 @@
+// Components
+import Navbar from "../components/Navbar";
+
+// CSS
+import "./History.css";
+
+// dummy data
+import database from "./historyDummyData";
+
+const TradeLineItem = (props) => {
+  return (
+    <div className="trade-line">
+      <p className="trade-info mb-0">{props.stockName}</p>
+      <p className="trade-info mb-0">
+        {new Date(props.tradeTime).toLocaleDateString()}
+      </p>
+      <p className="trade-info mb-0">{props.tradePrice}</p>
+      <p className="trade-info mb-0">{props.platform}</p>
+    </div>
+  );
+};
+
 const History = () => {
-  return <h1>History</h1>;
+  return (
+    <div>
+      <Navbar />
+      <div className="col nav-margin">
+        {/* Input here */}
+        <div>
+          <h1>Your Trades</h1>
+          <div className="trades-container">
+            <TradeLineItem
+              stockName="AAPL"
+              tradeTime={1702808925892}
+              tradePrice={12.8 + "USD"}
+              platform="thinkorswim"
+            />
+            {/* to-do, sort objects by date adn display from database */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default History;
---
+++ src/pages/historyDummyData.js
@@ -0,0 +1,25 @@
+const database = {
+  12345: {
+    trades: {
+      1: {
+        stockName: "AAPL",
+        date: 1702808925892,
+        price: 12.8,
+        currency: "USD",
+        platform: "thinkorswim",
+      },
+      2: {
+        stockName: "AAPL",
+        date: 1702808005892,
+        price: 12,
+        currency: "USD",
+        platform: "thinkorswim",
+      },
+    },
+    watchlist: {},
+    profileInfo: {},
+  },
+  abcde: { trades: {}, watchlist: {}, profileInfo: {} },
+};
+
+export default database;
