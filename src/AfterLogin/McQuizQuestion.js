7e1bb2602087023d778902557e4e6b7a4dc2254b
ianlauyin <ianlauyin@gmail.com>
2023-12-18 23:50:21 +0800

    add animate for next question

--- src/AfterLogin/McQuizQuestion.js
+++ src/AfterLogin/McQuizQuestion.js
@@ -1,18 +1,47 @@
-import { Card } from "@mui/material";
+import styled from "@emotion/styled";
+import { Button, Card } from "@mui/material";
 import { useState } from "react";
 
 export default function McQuizQuestion(props) {
   const [isCorrect, setIsCorrect] = useState(new Array(10).fill(false));
   const [isAnswered, setIsAnswered] = useState(new Array(10).fill(false));
-  const [currentQuestion, setCurrentQuestion] = useState(1);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [startAnimation, setAnimation] = useState(false);
+
+  const animate = `@keyframes next-question{
+    0%{right:${(currentQuestion - 1) * 100}%;}
+    100%{right:${currentQuestion * 100}%;}
+  }`;
+
+  const handleNextQuestion = () => {
+    setAnimation(true);
+    setCurrentQuestion((prev) => prev + 1);
+  };
 
   const questionsDisplay = props.questions.map((question, i) => {
     return (
-      <div className="quiz-sub-page" key={`question${i + 1}`}>
-        <Card className="quiz-card">{question.english}</Card>
+      <div
+        className="quiz-sub-page"
+        key={`question${i + 1}`}
+        style={{
+          animationName: startAnimation ? "next-question" : "none",
+          animationDuration: "0.5s",
+          postition: "relative",
+          right: `${currentQuestion * 100}%`,
+        }}
+        onAnimationEnd={() => setAnimation(false)}
+      >
+        <Card className="quiz-card">
+          {question.english}
+          <Button onClick={handleNextQuestion}>Right</Button>
+        </Card>
       </div>
     );
   });
-
-  return <div className="quiz-question">{questionsDisplay}</div>;
+  return (
+    <div className="quiz-question">
+      <style>{animate}</style> {/*put the animate style into use*/}
+      {questionsDisplay}
+    </div>
+  );
 }
