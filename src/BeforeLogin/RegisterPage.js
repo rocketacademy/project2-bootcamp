173c2aecd0f8477aea2ad3947eb48b8e309e3f04
Mannushka <zaimar1910@gmail.com>
2023-12-18 23:18:50 +0800

    add saving user to state logic (line 31-38)

--- src/BeforeLogin/RegisterPage.js
+++ src/BeforeLogin/RegisterPage.js
@@ -1,7 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
 
 // need to add logic to Register with firebase auth
 //After register into the auth, return to "/"
@@ -10,6 +13,7 @@ export default function RegisterPage(props) {
   const [password, setPassword] = useState("");
   const [errorCode, setErrorCode] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState("");
   const navi = useNavigate();
   const register = async () => {
     await createUserWithEmailAndPassword(auth, email, password)
@@ -23,6 +27,16 @@ export default function RegisterPage(props) {
         setErrorMessage(error.message);
       });
   };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      // If user is logged in, save logged-in user to state
+      if (user) {
+        setUser(user);
+        console.log(user);
+      }
+    });
+  });
 
   return (
     <div className="App">
@@ -47,7 +61,7 @@ export default function RegisterPage(props) {
         <p>{errorMessage && `Error message: ${errorMessage}`}</p>
       </div>
       <div className="currUser">
-        {auth.currentUser ? auth.currentUser.email : null}
+        {user ? <span> Logged in user: {user.email} </span> : null}
       </div>
     </div>
   );
