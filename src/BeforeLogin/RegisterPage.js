055b2f7e30ac534ab405722f304a48fc1ed09585
Mannushka <zaimar1910@gmail.com>
2023-12-18 23:57:46 +0800

    add name field + labels for each input

--- src/BeforeLogin/RegisterPage.js
+++ src/BeforeLogin/RegisterPage.js
@@ -3,12 +3,14 @@ import { auth } from "../firebase";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
+  updateProfile,
 } from "firebase/auth";
 import { useState, useEffect } from "react";
 
 // need to add logic to Register with firebase auth
 //After register into the auth, return to "/"
 export default function RegisterPage(props) {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [errorCode, setErrorCode] = useState("");
@@ -16,10 +18,14 @@ export default function RegisterPage(props) {
   const [user, setUser] = useState("");
   const navi = useNavigate();
   const register = async () => {
-    await createUserWithEmailAndPassword(auth, email, password)
+    await createUserWithEmailAndPassword(auth, email, password, name)
       .then(() => {
+        updateProfile(auth.currentUser, {
+          displayName: name,
+        });
         setEmail("");
         setPassword("");
+        setName("");
         navi("/");
       })
       .catch((error) => {
@@ -33,27 +39,42 @@ export default function RegisterPage(props) {
       // If user is logged in, save logged-in user to state
       if (user) {
         setUser(user);
-        console.log(user);
       }
     });
   });
 
   return (
     <div className="App">
-      <input
-        type="text"
-        name="email"
-        placeholder="Enter your email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      ></input>
-      <input
-        type="text"
-        name="password"
-        placeholder="Enter your password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      ></input>
+      <label>
+        Name or nickname:
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter your name or nickname"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        Email:
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        Password:
+        <input
+          type="text"
+          name="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </label>
       <button onClick={register}>Register</button>
 
       <div className="errorMessage">
@@ -61,7 +82,7 @@ export default function RegisterPage(props) {
         <p>{errorMessage && `Error message: ${errorMessage}`}</p>
       </div>
       <div className="currUser">
-        {user ? <span> Logged in user: {user.email} </span> : null}
+        {user ? <span> Logged in user: {user.displayName} </span> : null}
       </div>
     </div>
   );
