140b614c4dec0105119a89ab0fa4bfe915c6c728
ianlauyin <ianlauyin@gmail.com>
2023-12-20 22:32:12 +0800

    Add exit questions logic in quesiton header

--- src/AfterLogin/QuizComponent/McQuizHeader.js
+++ src/AfterLogin/QuizComponent/McQuizHeader.js
@@ -1,13 +1,34 @@
-import { LinearProgress } from "@mui/material";
+import { Button, Dialog, LinearProgress } from "@mui/material";
 import DisabledByDefaultOutlinedIcon from "@mui/icons-material/DisabledByDefaultOutlined";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function McQuizHeader(props) {
+  const [dialog, setDialog] = useState(false);
+  const navi = useNavigate();
+
   return (
     <div className="quiz-header">
+      <Dialog open={dialog}>
+        <div className="dialog">
+          Are you sure you want to exit?
+          <p>All progress will not be saved</p>
+          <div className="dialog-button-div">
+            <Button variant="contained" onClick={() => navi("/")}>
+              yes
+            </Button>
+            <Button variant="contained" onClick={() => setDialog(false)}>
+              no
+            </Button>
+          </div>
+        </div>
+      </Dialog>
       <div className="quiz-subheader">
         <h2>{props.questions[props.currentQuestion].deckName}</h2>
-        <DisabledByDefaultOutlinedIcon />
-        {/* Need to add confirmation leaving dialog for this icon*/}
+        <DisabledByDefaultOutlinedIcon
+          onClick={() => setDialog(true)}
+          className="exit-button"
+        />
       </div>
       <span className="progress-number">{props.currentQuestion + 1}/10</span>
       <LinearProgress
