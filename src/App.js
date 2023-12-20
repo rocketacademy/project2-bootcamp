2371bd59af57674db02181f5ab5628965bf786e5
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2023-12-20 22:20:22 +0800

    Enabled map rendering with @react-google-maps/api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import React from "react";
-import logo from "./logo.png";
-import "./App.css";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    );
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+const center = {
+  lat: 1.35313, // default latitude
+  lng: 103.81404, // default longitude
+};
+
+const App = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  if (loadError) {
+    return <div>Error loading maps</div>;
   }
-}
+
+  if (!isLoaded) {
+    return <div>Loading maps</div>;
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        mapId="6da2495ffc989dca"
+        zoom={12}
+        center={center}
+      >
+        <Marker position={center} />
+        <Marker
+          position={{ lat: 1.40058, lng: 103.90899 }}
+          onClick={() => {
+            console.log("Marker clicked!");
+            //TODO: Insert OpenAI API call here
+          }}
+        />
+      </GoogleMap>
+    </div>
+  );
+};
 
 export default App;
