01ac188004d90bd9d0d3afeb7739ee6cea9523b7
magiicloud <hongyun_rc@outlook.com>
2023-12-21 09:52:40 +0800

    added browser router, routes, and links to index.js. added new file CourseForm.js into pages folder.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { CourseForm } from "./pages/CourseForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Link to="/courseform">Create Course</Link>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/courseform" element={<CourseForm />} />
+    </Routes>
+  </BrowserRouter>
 );
