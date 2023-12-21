e55191ae8cdf9078d7abddc3b85a3c1b2e7510c1
magiicloud <hongyun_rc@outlook.com>
2023-12-21 14:31:18 +0800

    added form inputs and file upload components into CourseForm.js

--- src/pages/CourseForm.js
+++ src/pages/CourseForm.js
@@ -1,1 +1,43 @@
-export const CourseForm = () => {};
+export const CourseForm = () => {
+  return (
+    <>
+      <div className="">
+        <p className="text-xl">Create A Course</p>
+
+        <form>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Course Title</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Course Description</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Upload File</span>
+            </div>
+            <input
+              type="file"
+              className="file-input file-input-bordered w-full max-w-xs"
+            />
+          </label>
+        </form>
+      </div>
+    </>
+  );
+};
