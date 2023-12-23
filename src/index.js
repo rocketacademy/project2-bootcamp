2f74e099a6fc6a74870399f72f20ba8e8b65c817
cssml-vrs <matthew.liew@yahoo.com>
2023-12-24 00:55:38 +0800

    Instagram base

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.css";
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 import "./index.css";
-import App from "./App";
-
-// Pages
-import History from "./pages/History";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  { path: "/history", element: <History /> },
-]);
+import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(<App />);
