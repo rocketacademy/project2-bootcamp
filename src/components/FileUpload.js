bfa14581a286d9434fe28f991f0c3d2786dce92f
magiicloud <hongyun_rc@outlook.com>
2023-12-29 22:47:08 +0800

    added firebase functions to FileUpload

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,15 +1,45 @@
+import { storage } from "../firebase";
+import {
+  ref as storageRef,
+  uploadBytes,
+  getDownloadURL,
+} from "firebase/storage";
+import { push, ref, set } from "firebase/database";
+import { db } from "../firebase";
 import { useState } from "react";
 
 export const FileUpload = () => {
+  const STORAGE_KEY = `/courseMaterials`;
+
   const [fileInputFile, setFileInputFile] = useState(null);
 
   const handleFileUpload = (e) => {
     setFileInputFile(e.target.files[0]);
   };
 
+  const getURL = (url) => {
+    const fileUploadRef = ref(db, STORAGE_KEY);
+    const newFileUploadRef = push(fileUploadRef);
+    set(newFileUploadRef, {
+      url: url,
+    });
+  };
+
+  const uploadFile = () => {
+    const fullStorageRef = storageRef(
+      storage,
+      STORAGE_KEY + "_" + fileInputFile.name
+    );
+    uploadBytes(fullStorageRef, fileInputFile).then((snapshot) => {
+      getDownloadURL(fullStorageRef, fileInputFile.name).then((url) => {
+        getURL(url);
+      });
+    });
+  };
+
   return (
     <>
-      <label className="form-control w-full">
+      <label className="form-control w-full mb-2">
         <div className="label">
           <span className="label-text">Upload Course Materials</span>
         </div>
@@ -19,7 +49,9 @@ export const FileUpload = () => {
           onChange={handleFileUpload}
         />
       </label>
-      <div className="btn w-full">Upload</div>
+      <div className="btn w-full" onClick={uploadFile}>
+        Upload
+      </div>
     </>
   );
 };
