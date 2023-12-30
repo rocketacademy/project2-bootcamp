9dc3a852eb356ac1cd53d709663ab28968e3197e
Mannushka <zaimar1910@gmail.com>
2023-12-30 19:56:03 +0800

    added login logic

--- src/BeforeLogin/SignInPage.js
+++ src/BeforeLogin/SignInPage.js
@@ -1,13 +1,78 @@
 import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+import {
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  updateProfile,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
 
 // need to add logic to Sign in with firebase auth
 //After login into the auth, return to "/"
 export default function SignInPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorCode, setErrorCode] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [user, setUser] = useState("");
   const navi = useNavigate();
-
+  const logIn = async () => {
+    await signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        setEmail("");
+        setPassword("");
+        setName("");
+        navi("/");
+      })
+      .catch((error) => {
+        setErrorCode(error.code);
+        setErrorMessage(error.message);
+      });
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      // If user is logged in, save logged-in user to state
+      if (user) {
+        setUser(user);
+        // console.log(user);
+      }
+    });
+  });
   return (
     <div className="App">
-      <button onClick={() => navi("/")}>Sign in</button>
+      <label>
+        Email:
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+      </label>
+      <label>
+        Password:
+        <input
+          type="text"
+          name="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </label>
+      <button onClick={logIn}>Log in</button>
+
+      <div className="errorMessage">
+        <p>{errorCode && `Error code: ${errorCode}`}</p>
+        <p>{errorMessage && `Error message: ${errorMessage}`}</p>
+      </div>
+      <div className="currUser">
+        {user ? <span> Logged in user: {user.email} </span> : null}
+      </div>
     </div>
   );
 }
