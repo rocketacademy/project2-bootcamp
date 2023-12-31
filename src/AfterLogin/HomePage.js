1e988a06336eb16c4a1b33067fbebf99d58d40cb
Mannushka <zaimar1910@gmail.com>
2023-12-31 16:10:13 +0800

    edtited the greeting for user

--- src/AfterLogin/HomePage.js
+++ src/AfterLogin/HomePage.js
@@ -1,7 +1,15 @@
 import { useOutletContext } from "react-router-dom";
+import { auth } from "../firebase";
+
 //Take the user data from App.js state
 
 export default function HomePage() {
   const [user, setUser] = useOutletContext();
-  return <div>Hi, user.</div>;
+
+  return (
+    <div>
+      Hi, {auth.currentUser.displayName ? auth.currentUser.displayName : "user"}
+      .
+    </div>
+  );
 }
