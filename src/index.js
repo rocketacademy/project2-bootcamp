f2e5948efc32b5e41f981c6db1d062d183dab868
qiannnly <qianlinggg@gmail.com>
2024-01-02 20:29:10 +0800

    updated features

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import { CourseForm } from "./pages/CourseForm";
+// import { CourseForm } from "./pages/CourseForm";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Link to="/courseform">Create Course</Link>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/courseform" element={<CourseForm />} />
-    </Routes>
-  </BrowserRouter>
+  <App />
+  // <BrowserRouter>
+  //   <Link to="/courseform">Create Course</Link>
+  //   <Routes>
+  // <Route path="/" element={<App />} />
+  // {/* <Route path="/courseform" element={<CourseForm />} /> */}
+  //   </Routes>
+  // </BrowserRouter>
 );
