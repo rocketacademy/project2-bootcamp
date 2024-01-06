eaebb3132093b640c852fc386d8ae7a6067037ec
magiicloud <hongyun_rc@outlook.com>
2024-01-06 12:45:56 +0800

    removed default styling. not sure why but the prev deleted QuizForm.js and MCQ.js reappeared after previously deleting it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import logo from "./logo.png";
 import "./App.css";
 import Auth from "./pages/Auth";
 import Teacher from "./pages/Teacher";
@@ -11,22 +9,16 @@ import { AttendanceGForm } from "./pages/AttendanceGForm";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Auth />} />
-            <Route path="teacher" element={<Teacher />} />
-            <Route path="courseform" element={<CourseForm />} />
-            <Route path="attendance" element={<Attendance />} />
-            <Route path="attendanceform" element={<AttendanceGForm />} />
-          </Routes>
-        </BrowserRouter>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="teacher" element={<Teacher />} />
+          <Route path="courseform" element={<CourseForm />} />
+          <Route path="attendance" element={<Attendance />} />
+          <Route path="attendanceform" element={<AttendanceGForm />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
