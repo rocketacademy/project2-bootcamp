9a87c01323efe0c717937dc84ac05643d1114b2e
ianlauyin <ianlauyin@gmail.com>
2024-01-07 16:04:59 +0800

    Add answer in M&M

--- src/AfterLogin/QuizComponent/MixAndMatch/MixAndMatchQuizQuestion.js
+++ src/AfterLogin/QuizComponent/MixAndMatch/MixAndMatchQuizQuestion.js
@@ -5,16 +5,30 @@ export default function MixAndMatchQuizQuestion(props) {
   console.log(props.questions);
   const questionDisplay = props.questions.map((question) => {
     return (
-      <div className="mix-and-match-question-pair" key={question.cardID}>
+      <div
+        className="mix-and-match-question-pair"
+        key={`english${question.cardID}`}
+      >
         <Card className="mix-and-match-question-card">{question.english}</Card>
         <Card className="mix-and-match-question-card"></Card>
       </div>
+    );
+  });
+  const answerDisplay = props.questions.map((question) => {
+    return (
+      <Card
+        className="mix-and-match-question-card"
+        key={`spanish${question.cardID}`}
+      >
+        {question.spanish}
+      </Card>
     );
   });
   return (
     <div className="page">
       <MixAndMatchQuizHeader />
       <div className="mix-and-match-question">{questionDisplay}</div>
+      <div className="mix-and-match-answer">{answerDisplay}</div>
     </div>
   );
 }
