605c95a4bb17192bb734679d4ea41d5de95183c0
ianlauyin <ianlauyin@gmail.com>
2024-01-07 15:57:13 +0800

    Add qeustion in M&M

--- src/AfterLogin/QuizComponent/MixAndMatch/MixAndMatchQuizQuestion.js
+++ src/AfterLogin/QuizComponent/MixAndMatch/MixAndMatchQuizQuestion.js
@@ -1,10 +1,20 @@
+import { Card } from "@mui/material";
 import MixAndMatchQuizHeader from "./MixAndMatchQuizHeader";
 
 export default function MixAndMatchQuizQuestion(props) {
+  console.log(props.questions);
+  const questionDisplay = props.questions.map((question) => {
+    return (
+      <div className="mix-and-match-question-pair" key={question.cardID}>
+        <Card className="mix-and-match-question-card">{question.english}</Card>
+        <Card className="mix-and-match-question-card"></Card>
+      </div>
+    );
+  });
   return (
     <div className="page">
       <MixAndMatchQuizHeader />
-      Somethings
+      <div className="mix-and-match-question">{questionDisplay}</div>
     </div>
   );
 }
