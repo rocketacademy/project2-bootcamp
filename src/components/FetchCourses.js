148aa0b3b20812bdbae1edf8a2cbca889830affb
magiicloud <hongyun_rc@outlook.com>
2024-01-14 10:24:17 +0800

    refactored fetching of data into own component

--- src/components/FetchCourses.js
+++ src/components/FetchCourses.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import { ref, onChildAdded, off } from "firebase/database";
+import { db } from "../firebase";
+import { useState, useEffect } from "react";
 
 export const fetchAttemptedCourses = async (
   spreadsheetId,
@@ -24,4 +27,75 @@ export const fetchAttemptedCourses = async (
   }
 
   return attemptedCoursesResults;
+};
+
+export const useCourseData = () => {
+  const DB_COURSE_KEY = "courses";
+  const DB_STORAGE_KEY = "courseMaterials";
+  const coursesRef = ref(db, DB_COURSE_KEY);
+  const courseMaterialsRef = ref(db, DB_STORAGE_KEY);
+  const [courseMap, setCourseMap] = useState(new Map());
+  const [courseMaterialsMap, setCourseMaterialsMap] = useState(new Map());
+
+  useEffect(() => {
+    const handleCourses = (data) => {
+      const {
+        courseDescription,
+        courseID,
+        courseTitle,
+        createdDate,
+        dueDate,
+        quizLink,
+      } = data.val();
+      courseMap.set(courseID, {
+        courseDescription,
+        courseTitle,
+        courseID,
+        createdDate,
+        dueDate,
+        quizLink,
+      });
+      setCourseMap(
+        (prevMap) =>
+          new Map(
+            prevMap.set(courseID, {
+              courseDescription,
+              courseTitle,
+              courseID,
+              createdDate,
+              dueDate,
+              quizLink,
+            })
+          )
+      );
+    };
+
+    const handleCourseMaterials = (data) => {
+      const { courseID, fileName, url } = data.val();
+      courseMaterialsMap.set(data.key, {
+        fileName,
+        url,
+      });
+      setCourseMaterialsMap(
+        (prevMap) =>
+          new Map(
+            prevMap.set(data.key, {
+              courseID,
+              fileName,
+              url,
+            })
+          )
+      );
+    };
+
+    onChildAdded(coursesRef, handleCourses);
+    onChildAdded(courseMaterialsRef, handleCourseMaterials);
+
+    return () => {
+      off(coursesRef, handleCourses);
+      off(courseMaterialsRef, handleCourseMaterials);
+    };
+  }, []);
+
+  return { courseMap, courseMaterialsMap };
 };
