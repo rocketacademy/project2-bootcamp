2ebee31bab2296890d087f382ffed587d755d590
charlesleezhaoyi <charlesleezhaoyi@gmail.com>
2024-01-17 00:21:04 +0800

    Reinstated Forgot Password code

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,54 +1,67 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useState } from 'react';
-import { auth } from '../firebase';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useState } from "react";
+import { auth } from "../firebase";
 import {
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-} from 'firebase/auth';
+  sendPasswordResetEmail,
+} from "firebase/auth";
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { Card } from "@mui/material";
 
 const defaultTheme = createTheme();
 
 export default function SignIn() {
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const signUp = async () => {
     try {
       const user = await createUserWithEmailAndPassword(auth, email, password);
       console.log(user);
-      setEmail('');
-      setPassword('');
+      setEmail("");
+      setPassword("");
 
-      navigate('/onboarding');
+      navigate("/onboarding");
     } catch (error) {
-      console.error('Error signing up', error);
+      console.error("Error signing up", error);
     }
   };
 
   const signIn = async () => {
     const user = await signInWithEmailAndPassword(auth, email, password);
     console.log(user);
-    setEmail('');
-    setPassword('');
+    setEmail("");
+    setPassword("");
 
-    navigate('/');
+    navigate("/");
+  };
+
+  const forgotPassword = async () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        console.log("Email sent");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
   };
 
   // const handleSubmit = (event) => {
@@ -64,70 +77,81 @@ export default function SignIn() {
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box
+        <Card
           sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            width: 500,
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={signIn}
-            >
-              Sign In
-            </Button>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 1, mb: 1 }}
-              onClick={signUp}
-            >
-              Sign Up
-            </Button>
-            <Grid container></Grid>
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <FormControlLabel
+                control={<Button value="forgot" color="secondary" />}
+                label="Forgot Password"
+                onClick={forgotPassword}
+              ></FormControlLabel>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={signIn}
+              >
+                Sign In
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 1, mb: 1 }}
+                onClick={signUp}
+              >
+                Sign Up
+              </Button>
+              <Grid container></Grid>
+            </Box>
           </Box>
-        </Box>
+        </Card>
       </Container>
     </ThemeProvider>
   );
