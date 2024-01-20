ebc615ce331d0e7662831801e49158be5c039f23
magiicloud <hongyun_rc@outlook.com>
2024-01-20 20:30:37 +0800

    added AuthProvider.js to pass login details and role

---
+++ src/pages/AuthProvider.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { auth, db } from "../firebase";
+import { ref, get } from "firebase/database";
+
+export const AuthContext = React.createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [authState, setAuthState] = useState({
+    currentUser: null,
+    role: null,
+  });
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const role = await getUserRole(user.uid);
+        setAuthState({
+          currentUser: user,
+          role: role,
+        });
+      } else {
+        setAuthState({
+          currentUser: null,
+          role: null,
+        });
+      }
+    });
+
+    // return () => {
+    //   unsubscribe();
+    // };
+  }, []);
+
+  const getUserRole = async (uid) => {
+    const studentRef = ref(db, `Student/${uid}`);
+    const teacherRef = ref(db, `Teacher/${uid}`);
+
+    const studentSnapshot = await get(studentRef);
+    const teacherSnapshot = await get(teacherRef);
+
+    if (studentSnapshot.exists()) {
+      return "student";
+    } else if (teacherSnapshot.exists()) {
+      return "teacher";
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={authState}>{children}</AuthContext.Provider>
+  );
+};
