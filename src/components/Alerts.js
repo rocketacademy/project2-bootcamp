a7f8e47a5ca461f6aff53f198815a9753c421284
magiicloud <hongyun_rc@outlook.com>
2024-01-23 19:49:40 +0800

    amended icon for error alert

--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -11,7 +11,7 @@ export const AlertError = ({ alertText }) => {
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
-          d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
         />
       </svg>
       <span>{alertText}</span>
