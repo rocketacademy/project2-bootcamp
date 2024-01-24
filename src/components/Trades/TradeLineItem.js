2df5225b115adba88c18e17c8a3c449a90e2399d
patrickkok <pkok1996@gmail.com>
2024-01-24 22:38:17 +0800

    Remove unused component, change trade lines to match theme

--- src/components/Trades/TradeLineItem.js
+++ src/components/Trades/TradeLineItem.js
@@ -48,7 +48,7 @@ const TradeLineItem = (props) => {
           <g id="SVGRepo_iconCarrier">
             <path
               d="M12.2424 20H17.5758M4.48485 16.5L15.8242 5.25607C16.5395 4.54674 17.6798 4.5061 18.4438 5.16268V5.16268C19.2877 5.8879 19.3462 7.17421 18.5716 7.97301L7.39394 19.5L4 20L4.48485 16.5Z"
-              stroke="black"
+              stroke="white"
               stroke-linecap="round"
               stroke-linejoin="round"
             ></path>
